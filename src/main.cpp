d746bcb4d55ff1e5af5e1302f8fbf3780ed32899
lostybtw <aarnav.chaplot@gmail.com>
2023-05-13 14:39:42 +0530

    episode 8

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,40 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
 
+struct ShaderProgramSource{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& file){
+	enum class shader_type{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1	
+	};
+	std::ifstream stream(file);
+	std::string line;
+	std::stringstream ss[2];
+	shader_type type = shader_type::NONE;
+	while(getline(stream, line)){
+		if(line.find("#shader") != std::string::npos){
+				if(line.find("vertex") != std::string::npos){
+					type = shader_type::VERTEX;
+				}
+				else if(line.find("fragment") != std::string::npos){
+					type = shader_type::FRAGMENT;
+				}
+		}
+		else{
+			ss[int(type)] << line << '\n';
+		}
+	}
+	return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(const std::string& source , unsigned int type){
 	unsigned int id = glCreateShader(type);
@@ -78,24 +109,9 @@ int main(void)
     glVertexAttribPointer(0,2,GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 	glEnableVertexAttribArray(0);
 
-	std::string vertexshader = "#version 330 core\n"
-		"\n"
-		"layout(location=0) in vec4 position;"
-		"\n"
-		"void main(){\n"
-		"	gl_Position = position;\n"
-		"}";
-
-	std::string fragmentshader = "#version 330 core\n"
-		"\n"
-		"layout(location=0) out vec4 color;"
-		"\n"
-		"void main(){\n"
-		"	color = vec4(0.5, 1.0, 0.5, 1.0);\n"
-		"}";
-
-	unsigned int Shader = CreateShader(vertexshader, fragmentshader);
-	glUseProgram(Shader);
+	ShaderProgramSource	src = ParseShader("res/basic.shader");
+	unsigned int Shader = CreateShader(src.VertexSource, src.FragmentSource);
+	glUseProgram(Shader); 
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
